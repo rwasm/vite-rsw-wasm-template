7bb2e333552dffe25aca73a2fabac283b78f43f3
lencx <cxin1314@gmail.com>
2022-05-19 17:31:19 +0800

    chore: update rsw

--- wasm-vue/src/components/HelloWasm.vue
+++ wasm-vue/src/components/HelloWasm.vue
@@ -1,10 +1,11 @@
 <template>
-  <button @click="greet()">hello wasm</button>
+  <button @click="greet('heloo')">hello wasm</button>
 </template>
 
 <script setup lang="ts">
 import { defineComponent } from 'vue'
 import init, { greet } from 'rsw-hello'
+
 
 init()
 </script>
